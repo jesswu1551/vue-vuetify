1ecbcc60d0afc3a339193bb671257c9d012c9593
Jia Yu Wu <jiayuwu@Jesss-MacBook-Pro.local>
2022-04-05 01:55:12 +0800

    update datatable component layout

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,31 +1,39 @@
 <template>
-<div>
-  <table id="firstTable">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Phone</th>
-        <th>Profession</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="row in rows" :key="row.id">
-        <td>{{row.id}}</td>
-        <td>{{row.name}}</td>
-        <td>{{row.phone}}</td>
-        <td>{{row.profession}}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="12">
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="12">
+        <v-data-table
+          :headers="headers"
+          :items="employee"
+          :search="search"
+          :items-per-page="-1"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      rows: [
+      search: '',
+      headers: [
+        { text: 'ID', align: 'start', sortable: 'true', value: 'id' },
+        { text: 'Name', align: 'start', sortable: 'true', value: 'name' },
+        { text: 'Phone', align: 'start', sortable: 'true', value: 'phone' },
+        { text: 'Profession', align: 'start', sortable: 'true', value: 'profession' },
+      ],
+      employee: [
         { id: 1, name: "Chandler Bing", phone: '305-917-1301', profession: 'IT Manager' },
         { id: 2, name: "Ross Geller", phone: '210-684-8953', profession: 'Paleontologist' },
         { id: 3, name: "Rachel Green", phone: '765-338-0312', profession: 'Waitress'},
