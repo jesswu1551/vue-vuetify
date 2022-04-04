5fa4c08898be112becd786a4ae5737bc866b9a2d
Jia Yu Wu <jiayuwu@Jesss-MacBook-Pro.local>
2022-04-05 01:31:31 +0800

    update grid layout and form input fields style

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-    <input placeholder="Name">
-    <input placeholder="Phone">
-    <input placeholder="Profession">
-    <button>Send</button>
-  </div>
+<v-form>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="3">
+        <v-text-field label="Name" value=""></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="3">
+        <v-text-field label="Phone" value=""></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="3">
+        <v-text-field label="Profession" value=""></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="3">
+        <v-btn class="mt-3" color="primary">Send</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</v-form>
 </template>
